a8c5c871a34d30a06e8f0e02ddc56d59f0407889
Thomas Abbink <t.g.abbink@gmail.com>
2023-01-10 13:38:26 +0100

    moved the server into a different dir

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+const port  = 8080
+
+app.get('/', (req, res) => {
+
+    const file = fs.readFileSync('server/data.txt', 'utf-8')
+    const cameras = file.split(/\r?\n/).map(line => {
+
+        if(line.substring(0, 3) !== "UTR"){
+            return null
+        }
+
+        const lineSections = line.split(";")
+        // will break if the code format changes
+        const camera =  {
+            code: lineSections[0].substring(0, 10),
+            number: lineSections[0].substring(7, 10),
+            name: lineSections[0].substring(11),
+            latitude:lineSections[1],
+            longitude: lineSections[2]
+        }
+        return camera
+    }).filter(Boolean)
+
+    res.send(JSON.stringify(cameras))
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
