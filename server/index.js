e1df333e0db947035ce9bbf3c6b97d694917ae54
Thomas Abbink <t.g.abbink@gmail.com>
2023-01-10 15:21:44 +0100

    added cors to server

--- server/index.js
+++ server/index.js
@@ -1,33 +1,35 @@
-const express = require('express')
-const fs = require('fs')
-
+const express = require("express")
+const fs = require("fs")
+const cors = require("cors")
 const app = express()
-const port  = 8080
+const port = 8080
+app.use(cors())
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  const file = fs.readFileSync("data.txt", "utf-8")
+  const cameras = file
+    .split(/\r?\n/)
+    .map((line) => {
+      if (line.substring(0, 3) !== "UTR") {
+        return null
+      }
 
-    const file = fs.readFileSync('server/data.txt', 'utf-8')
-    const cameras = file.split(/\r?\n/).map(line => {
+      const lineSections = line.split(";")
+      // will break if the code format changes
+      const camera = {
+        code: lineSections[0].substring(0, 10),
+        number: lineSections[0].substring(7, 10),
+        name: lineSections[0].substring(11),
+        latitude: lineSections[1],
+        longitude: lineSections[2],
+      }
+      return camera
+    })
+    .filter(Boolean)
 
-        if(line.substring(0, 3) !== "UTR"){
-            return null
-        }
-
-        const lineSections = line.split(";")
-        // will break if the code format changes
-        const camera =  {
-            code: lineSections[0].substring(0, 10),
-            number: lineSections[0].substring(7, 10),
-            name: lineSections[0].substring(11),
-            latitude:lineSections[1],
-            longitude: lineSections[2]
-        }
-        return camera
-    }).filter(Boolean)
-
-    res.send(JSON.stringify(cameras))
+  res.send(JSON.stringify(cameras))
 })
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+  console.log(`listening on port ${port}`)
 })
